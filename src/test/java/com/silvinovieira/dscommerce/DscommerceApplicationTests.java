17e7b6e2b376f4100599c00fdb6e9dd002fa5542
vinovieira <127680187+vinovieira@users.noreply.github.com>
2024-12-28 19:09:57 -0400

    create initial project

---
+++ src/test/java/com/silvinovieira/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvinovieira.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
