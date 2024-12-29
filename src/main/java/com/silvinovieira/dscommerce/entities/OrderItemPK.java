5eac0e515220d8589bc21c190c83e05a52b4bb7c
vinovieira <127680187+vinovieira@users.noreply.github.com>
2024-12-28 23:58:34 -0400

    Category, Product, OrderItem, OrderItemPK

---
+++ src/main/java/com/silvinovieira/dscommerce/entities/OrderItemPK.java
@@ -0,0 +1,40 @@
+package com.silvinovieira.dscommerce.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Embeddable
+public class OrderItemPK {
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    public OrderItemPK() {}
+
+    public OrderItemPK(Product product, Order order) {
+        this.product = product;
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+}
